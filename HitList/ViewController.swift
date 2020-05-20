d928acfd0d7bb579414c08fb55ed6294e692b70e
Jurate Kazlauskaite <kazlauskaite.jura@gmail.com>
2020-05-20 15:46:24 +0100

    Store and retrieve names from the names: [String]. - Create a names: [String] - Set UITableView: register the UITableViewCell class with the table view, create UITableViewDataSource methods (numberOfRowsInSection, cellForRowAt indexPath) - Create addName button. Display an alert with a text box and two action buttons: Save and Cancel when the button is pressed

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -9,12 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var names: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "The List"
+        // Register the UITableViewCell class with the table view
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
-
-
+    
+    // addName pressed
+    // Display an alert with a text box and two action buttons: Save and Cancel
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "New Name", message: "Add a new Name", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+            action in
+            
+            guard let textField = alert.textFields?.first,
+                let nameToSave = textField.text else {
+                    return
+            }
+            
+            self.names.append(nameToSave)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField()
+        
+        // Save and Cancel buttons
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
 }
 
+// MARK: - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+    
+    // Set as many rows as there are names
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    // Display a cell for each row with the corresponding string from the names array
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+}
