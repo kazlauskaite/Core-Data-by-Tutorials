91be86a1a5bbe187b5d31e9fc9a8394e917f4e9e
Jurate Kazlauskaite <kazlauskaite.jura@gmail.com>
2020-05-20 19:59:40 +0100

    Persistently store and retrieve names from the disk. - Import CoreData module - Replace names: [String] property to people: [NSManagedObject] - Model data in .xcdatamodeld file: create "person" entity and "name" attribute - Create save(name: String) method to save names to Core Data store - Fetch data from Core Data in viewWillAppear

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     
-    var names: [String] = []
+    var people: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,25 @@ class ViewController: UIViewController {
         title = "The List"
         // Register the UITableViewCell class with the table view
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Get a reference to the app delegate to access NSPersistentContainer's context property
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        // Creating a fetch request with the criteria to retrieve all objects of "Person" entity
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        do {
+            // fetch(_:) returns an array of managed objects meeting the criteria specified by the fetch request
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
     }
     
     // addName pressed
@@ -36,7 +56,7 @@ class ViewController: UIViewController {
                     return
             }
             
-            self.names.append(nameToSave)
+            self.save(name: nameToSave)
             self.tableView.reloadData()
         }
         
@@ -50,23 +70,47 @@ class ViewController: UIViewController {
         
         present(alert, animated: true)
     }
+    
+    func save(name: String) {
+        // Get a reference to the appDelegate to access NSPersistentContainer's context property
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        // Create a new managed object and insert it into the managed object context
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        // Set the name attribute using key-value coding
+        person.setValue(name, forKeyPath: "name")
+        
+        // Commit changes to person and save to disk by calling save on the managed object context
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
 }
 
 // MARK: - UITableViewDataSource
 
 extension ViewController: UITableViewDataSource {
     
-    // Set as many rows as there are names
+    // Set as many rows as there are people
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
-    // Display a cell for each row with the corresponding string from the names array
+    // Display a cell for each row with the corresponding NSManagedObject from the people's array
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let person = people[indexPath.row]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
         return cell
     }
 }
