bd63718d6c464f5ca94cf7096f55a4ffcdbd9d75
Jurate Kazlauskaite <kazlauskaite.jura@gmail.com>
2020-05-20 13:14:25 +0100

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Jurate Kazlauskaite on 20/05/2020.
+//  Copyright © 2020 Jurate Kazlauskaite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
